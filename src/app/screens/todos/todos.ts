30baef82badd7dab9980dabed44d8e7ea1a71b3d
Danilo Righetto <danilonewtrue@gmail.com>
2025-11-03 05:38:32 -0300

    feat: adding the items to the application's local storage

--- src/app/screens/todos/todos.ts
+++ src/app/screens/todos/todos.ts
@@ -1,13 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { Title } from '../../components/shared/title/title';
 import { TodosService } from '../../services/todos.service';
+import { MatIconModule } from '@angular/material/icon'
 @Component({
   selector: 'app-todos',
-  imports: [Title],
+  imports: [Title, MatIconModule],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
 export class Todos {
   /* Injetando o serviço no componente */
   readonly todos = inject(TodosService);
+
+  /* Adicionando o Evento e o elemento HTML */
+  addTodo(event: Event, input: HTMLInputElement) {
+    event.preventDefault();
+    
+    const title = input.value.trim();
+    if (!title) return;
+
+    this.todos.add(title);
+    input.value = '';
+  }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,23 +1,55 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Todo } from '../model/todo.entity';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-  private readonly _items = signal<Array<Todo>>([
-    { id: crypto.randomUUID(), title: 'Implementar App Mobile', completed: false },
-    { id: crypto.randomUUID(), title: 'Estudar Angular Avançado', completed: true },
-    { id: crypto.randomUUID(), title: 'Revisar Código do Projeto', completed: false }
-  ]);
+  private readonly _items = signal<Array<Todo>>([]);
 
   readonly items = this._items.asReadonly();
 
+  constructor() {
+    this._load();
+
+    /* Atualiza os itens no localStorage sempre que os itens são modificados */
+    effect(() => {
+      const items = this._items();
+      localStorage.setItem('todos', JSON.stringify(items));
+    })
+  }
+
+  add(title: string) {
+    const newTodo: Todo = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false
+    };
+    this._items.update((items) => [...items, newTodo]);
+  }
+
   toggle(id: string) {
-    this._items.update((items) => 
-      items.map((item) => 
+    this._items.update((items) =>
+      items.map((item) =>
         item.id === id ? { ...item, completed: !item.completed } : item
       )
     )
+  }
+
+  remove(id: string) {
+    this._items.update((items) => items.filter((item) => item.id !== id));
+  }
+
+  private _load() {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      this._items.set(JSON.parse(storedTodos));
+    } else {
+      this._items.set([
+        { id: crypto.randomUUID(), title: 'Implementar App Mobile', completed: false },
+        { id: crypto.randomUUID(), title: 'Estudar Angular Avançado', completed: true },
+        { id: crypto.randomUUID(), title: 'Revisar Código do Projeto', completed: false }
+      ]);
+    }
   }
 }
